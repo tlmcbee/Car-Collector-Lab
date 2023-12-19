0695de2c9768fa7490395b496980076c13e13b16
tlmcbee <tlmcbeejr@gmail.com>
2023-12-19 14:11:11 -0800

    Initial Commit

---
+++ main_app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+cars = [
+  {'make': 'BMW', 'model': '328i', 'year': 1999, 'body_type': 'Sedan'},
+  {'make': 'Chevy', 'model': 'Traverse', 'year': 2014, 'body_type': 'Suv'},
+  {'make': 'Porsche', 'model': '911', 'year': 2020, 'body_type': 'Coupe'},
+  {'make': 'Volkswagen', 'model': 'Jetta', 'year': 2009, 'body_type': 'Wagon'},
+  {'make': 'Honda', 'model': 'Civic', 'year': 1996, 'body_type': 'Hatchback'},
+]
+
+# Create your views here.
