f2b549a62bd53b75229381618e6d30a23afe50f8
tlmcbee <tlmcbeejr@gmail.com>
2023-12-19 16:03:39 -0800

    Adds templates, updates url paths, updates views

--- main_app/views.py
+++ main_app/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
 
 cars = [
-  {'make': 'BMW', 'model': '328i', 'year': 1999, 'body_type': 'Sedan'},
-  {'make': 'Chevy', 'model': 'Traverse', 'year': 2014, 'body_type': 'Suv'},
-  {'make': 'Porsche', 'model': '911', 'year': 2020, 'body_type': 'Coupe'},
-  {'make': 'Volkswagen', 'model': 'Jetta', 'year': 2009, 'body_type': 'Wagon'},
-  {'make': 'Honda', 'model': 'Civic', 'year': 1996, 'body_type': 'Hatchback'},
+  {'make': 'BMW', 'model': '328i', 'year': 1999, 'color': 'Blue', 'body_type': 'Sedan'},
+  {'make': 'Chevy', 'model': 'Traverse', 'year': 2014, 'color': 'white', 'body_type': 'Suv'},
+  {'make': 'Porsche', 'model': '911', 'year': 2020, 'color': 'Teal', 'body_type': 'Coupe'},
+  {'make': 'Volkswagen', 'model': 'Jetta', 'year': 2009, 'color': 'Black', 'body_type': 'Wagon'},
+  {'make': 'Honda', 'model': 'Civic', 'year': 1996, 'color': 'Red', 'body_type': 'Hatchback'},
 ]
 
 # Create your views here.
+def home(request):
+  return render(request, 'home.html')
+
+def about(request):
+  return render(request, 'about.html')
+
+def car_index(request):
+  return render(request, 'cars/index.html', {
+    'cars': cars
+  })
