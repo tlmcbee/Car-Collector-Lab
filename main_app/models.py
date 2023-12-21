c964578b66bfc0c193bd73835d54c5b81086725e
tlmcbee <tlmcbeejr@gmail.com>
2023-12-20 16:56:17 -0800

    Adds CBV for create, update and delete

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Car(models.Model):
@@ -10,3 +11,6 @@ class Car(models.Model):
 
   def __str__(self):
     return f'{self.make} ({self.id})'
+  
+  def get_absolute_url(self):
+    return reverse('detail', kwargs={'car_id': self.id})
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Car
 
 # Create your views here.
@@ -19,3 +20,15 @@ def cars_detail(request, car_id):
   return render(request, 'cars/detail.html', {
     'car': car
   })
+
+class CarCreate(CreateView):
+  model = Car
+  fields = '__all__'
+
+class CarUpdate(UpdateView):
+  model = Car
+  fields = ['color', 'year', 'body_type']
+
+class CarDelete(DeleteView):
+  model = Car
+  success_url = '/cars'
