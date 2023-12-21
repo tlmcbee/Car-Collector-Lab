18faf8f949f99ade2c3b19a67fb8ea012a3cd90a
tlmcbee <tlmcbeejr@gmail.com>
2023-12-20 16:03:24 -0800

    Adds Car Model. Adds Detail template, view and url.

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Car(models.Model):
+  make = models.CharField(max_length=100)
+  model = models.CharField(max_length=100)
+  year = models.IntegerField()
+  color = models.CharField(max_length=100)
+  body_type = models.CharField(max_length=100)
+
+  def __str__(self):
+    return f'{self.make} ({self.id})'
--- main_app/views.py
+++ main_app/views.py
@@ -1,12 +1,5 @@
 from django.shortcuts import render
-
-cars = [
-  {'make': 'BMW', 'model': '328i', 'year': 1999, 'color': 'Blue', 'body_type': 'Sedan'},
-  {'make': 'Chevy', 'model': 'Traverse', 'year': 2014, 'color': 'white', 'body_type': 'Suv'},
-  {'make': 'Porsche', 'model': '911', 'year': 2020, 'color': 'Teal', 'body_type': 'Coupe'},
-  {'make': 'Volkswagen', 'model': 'Jetta', 'year': 2009, 'color': 'Black', 'body_type': 'Wagon'},
-  {'make': 'Honda', 'model': 'Civic', 'year': 1996, 'color': 'Red', 'body_type': 'Hatchback'},
-]
+from .models import Car
 
 # Create your views here.
 def home(request):
@@ -16,6 +9,13 @@ def about(request):
   return render(request, 'about.html')
 
 def car_index(request):
+  cars = Car.objects.all()
   return render(request, 'cars/index.html', {
     'cars': cars
+  })
+
+def cars_detail(request, car_id):
+  car = Car.objects.get(id=car_id)
+  return render(request, 'cars/detail.html', {
+    'car': car
   })
